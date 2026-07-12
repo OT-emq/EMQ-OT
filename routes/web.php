cbf23f81050c66c5cd202f2fe21a7059c53b0534
Leo <diegolyt29@gmail.com>
2026-07-12 11:57:34 -0500

    Implementacion de layout, actividades, plan diario, trabajadores y header; integracion de rutas

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
-});
+}); */
+
+Route::view('/', 'layouts.app')->name('index');
+
+Route::view('/trabajadores', 'pages.trabajadores.index')->name('trabajadores.index');
+Route::view('/actividades','pages.actividades.index')->name('actividades.index');
+Route::view('/plan-diario','pages.plan-diario.index')->name('plan.index');
