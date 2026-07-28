614b27a6abddfef625b37d1888cae337a8cf3c1d
Leo <diegolyt29@gmail.com>
2026-07-28 14:01:22 -0500

    Implementación de Inicio y agregación de controlador de inicio

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,15 @@
 
 use App\Models\DailyPlane;
 use App\Http\Controllers\DailyPlaneController;
+use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
 
 /* Route::get('/', function () {
     return view('welcome');
 }); */
 
-Route::view('/', 'layouts.app')->name('index');
+Route::redirect('/', '/inicio');
+Route::get('/inicio', [IndexController::class, 'index'])->name('index');
 
 Route::get('/plan-diario', [DailyPlaneController::class, 'index'])->name('plan.index');
 Route::view('/trabajadores', 'pages.workers.index')->name('workers.index');
